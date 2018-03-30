573cab562fd132896a0ffe6d50fdac3e46b23ae1
Dadadayuer <35052437+Dadadayuer@users.noreply.github.com>
2018-03-30 16:39:10 +0800

    这是练习了一下 求质数因子的算法

--- ConsoleApplication1/源.cpp
+++ ConsoleApplication1/源.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-
+using  namespace std;
 int main() {
-	std::cout << 123 << std::endl;
-std::cout << "这是一次新的尝试！" << std::endl;
-	
+//	cout << 123 << endl;
+//cout << "这是一次新的尝试！" <<endl;
+	/*int n;
+	cout << "请输入一个参数："<<endl;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+		while (n != i){
+			if (n%i == 0){
+				n = n / i;
+				cout << i<<"\t";
+			}
+			else{
+				break;
+			}
+		}
+		cout << n;*/
+	bool a;
+	a = 0 ^ 0;
+	cout << a;
 	return  0;
 }
